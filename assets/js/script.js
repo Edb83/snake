18d1384817aafe6015addd76c39383ec4de1c89a
Edb83 <ed3radley@gmail.com>
2020-06-08 13:47:27 +0000

    Added && ! to directions to prevent snake moving back into itself. Issue with food appearing outside border, as well as inside existing snake body

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,13 +35,13 @@ let direction = "left";
 document.addEventListener("keydown", keyDownHandler);
 
 function keyDownHandler(event) {
-    if(event.keyCode == 38) {
+    if(event.keyCode == 38 && direction != "down") {
         direction = "up"
-    } else if (event.keyCode == 40) {
+    } else if (event.keyCode == 40 && direction != "up") {
         direction = "down"
-    } else if (event.keyCode == 37) {
+    } else if (event.keyCode == 37 && direction != "right") {
         direction = "left"
-    } else if (event.keyCode == 39) {
+    } else if (event.keyCode == 39 && direction != "left") {
         direction = "right"
     };
 }
