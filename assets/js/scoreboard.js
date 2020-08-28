851a3cb22e9bd1cbaaac12539a0bf2d9f2f6146e
Edb83 <ed3radley@gmail.com>
2020-08-28 09:12:30 +0000

    Updated award text and refactored some code

--- assets/js/scoreboard.js
+++ assets/js/scoreboard.js
@@ -93,7 +93,7 @@ let scoreBoard = {
             scoreAwardText.insertAdjacentHTML("beforeend", `Maybe getting to 50 was good enough for you. `);
         }
         if (scoreRange(60, 69)) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `${convertSecondsToHms(stats.gameTimeInSeconds)} to score ${this.currentScore}? What a triumph. `);
+            scoreAwardText.insertAdjacentHTML("beforeend", `This is what cybernetic dreams are made of. `);
         }
         if (scoreRange(70, 79)) {
             scoreAwardText.insertAdjacentHTML("beforeend", `"I was distracted by the pretty colors!", I hear you wail. `);
@@ -104,13 +104,12 @@ let scoreBoard = {
         if (scoreRange(90, 99)) {
             scoreAwardText.insertAdjacentHTML("beforeend", `Did you consider persevering and making it to 100? `);
         }
-        if (this.currentScore >= 100 &&
-            this.currentScore < 125 &&
-            (this.currentHighScore < 100 || isNaN(this.currentHighScore))) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `That's quite the milestone you've hit.<br>And it only took you ${stats.gamesPlayedAllTime} attempts! `);
+        if (scoreRange(100, 124) &&
+            (this.currentScore > this.currentHighScore) {
+            scoreAwardText.innerHTML = `That's quite the milestone you've hit.<br>And it only took you ${stats.gamesPlayedAllTime} attempts! `;
         }
         else if (scoreRange(100, 124)) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `Overall you have smashed ${stats.pointsAllTime} blobs to smithereens. The Nanite Narwhal would be proud. `);
+            scoreAwardText.insertAdjacentHTML("beforeend", `In total you have smashed ${stats.pointsAllTime} blobs to smithereens. The Nanite Narwhal would be proud. `);
         }
         if (scoreRange(125, 149)) {
             scoreAwardText.insertAdjacentHTML("beforeend", `After ${convertSecondsToHms(stats.gameTimeAllTime)} of total play time, things have clicked. `);
@@ -119,16 +118,16 @@ let scoreBoard = {
             scoreAwardText.insertAdjacentHTML("beforeend", `The Digital Mongoose has been informed of your progress. `);
         }
         if (scoreRange(200, 299)) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `Definitely cheating. `);
+            scoreAwardText.innerHTML = `Definitely cheating. `;
         }
         if (scoreRange(300, 396)) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `Assuming you're not cheating, I'm impressed by your commitment and sorry that you have wasted your time. `);
+            scoreAwardText.innerHTML = `Assuming you're not cheating, I'm impressed by your commitment and sorry that you have wasted your time. `;
         }
         if (this.currentScore == 397) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `Congratulations.<br>You have completed the tutorial of Cyber Snake.<br>In Level 001 the food is invisible. You have 3 lives remaining.<br>Good luck. `);
+            scoreAwardText.innerHTML= `Congratulations.<br>You have completed the tutorial of Cyber Snake.<br>In Level 001 the food is invisible. You have 3 lives remaining.<br>Good luck. `;
         }
         if (this.currentScore > 397) {
-            scoreAwardText.insertAdjacentHTML("beforeend", `Is that even possible? `);
+            scoreAwardText.innerHTML= `Is that even possible? `;
         }
         if (this.currentScore === this.previousScore && this.currentScore !== 0) {
             scoreAwardText.innerHTML = `Oops you did it again. `;
@@ -139,7 +138,7 @@ let scoreBoard = {
             scoreAwardText.innerHTML = `${convertSecondsToHms(stats.gameTimeInSeconds)} to add a measly ${this.currentScore - this.currentHighScore} to your PB.<br> Yikes.`;
         }
         if (this.currentScore > this.previousScore && this.previousScore === 0) {
-            scoreAwardText.innerHTML = `Well, anything was an improvement on last time. Extra credit for testing the walls out though. `;
+            scoreAwardText.innerHTML = `Well, anything was an improvement on last time. Kudos for testing the collision detection out though. `;
         }
         if (this.previousScore - this.currentScore > 50) {
             scoreAwardText.insertAdjacentHTML("beforeend", `Try to remember what you did on your previous attempt. That was better. `);
